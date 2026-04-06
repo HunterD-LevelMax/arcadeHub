f1b9d76b6914736f179ecd4b231fc588fa8415fd
HunterD-LevelMax <warzvitaly@gmail.com>
2026-04-06 14:37:46 +0700

    update

--- js/game.js
+++ js/game.js
@@ -44,3 +44,39 @@ function initGameCommon(canvas, baseWidth, baseHeight, onResize) {
   resizeCanvas();
   window.addEventListener('resize', resizeCanvas);
 }
+
+// Cookie utilities
+function setCookie(name, value, days = 365) {
+  const date = new Date();
+  date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// High score management - saves to both localStorage and cookies
+function getHighScore(gameId) {
+  let score = localStorage.getItem(gameId + '_best');
+  if (score === null) {
+    score = getCookie(gameId + '_best');
+    if (score !== null) {
+      localStorage.setItem(gameId + '_best', score);
+    }
+  }
+  return parseInt(score || '0');
+}
+
+function setHighScore(gameId, score) {
+  localStorage.setItem(gameId + '_best', score);
+  setCookie(gameId + '_best', score);
+}
